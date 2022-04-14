47197f3f84719cd4f3a05daa01739448db91bb02
wxh <704276893@qq.com>
2022-04-14 14:43:15 +0800

    Initial commit

---
+++ Example/A_Category/HAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HAppDelegate.h
+//  A_Category
+//
+//  Created by wxh on 04/14/2022.
+//  Copyright (c) 2022 wxh. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
